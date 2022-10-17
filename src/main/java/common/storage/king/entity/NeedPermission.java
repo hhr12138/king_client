b9a0514e450be0e829aec2580ee6b6807cfc1b97
houhaoran <1448191711@qq.com>
2022-10-17 17:47:26 +0800

    feat: 鉴权服务相关接口

--- src/main/java/common/storage/king/entity/NeedPermission.java
+++ src/main/java/common/storage/king/entity/NeedPermission.java
@@ -23,6 +23,8 @@ public class NeedPermission implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private Long id;
+
     private String psm;
 
     /**
--- src/main/java/common/storage/king/entity/Role.java
+++ src/main/java/common/storage/king/entity/Role.java
@@ -1,4 +1,31 @@
 package common.storage.king.entity;
+import java.io.Serializable;
 
-public class Role {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author haoran hou
+ * @since 2022-10-01
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long companyId;
+
+    private Long departmentId;
+
+    private String roleName;
+
 }
